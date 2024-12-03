6003426d89f75bd53adf47bee4665cee1e06e4ba
Sudhanshu Garg <sudhanshug@gmail.com>
2024-12-02 23:13:53 -0800

    day3 both parts

--- 2024/Day3.java
+++ 2024/Day3.java
@@ -60,5 +60,70 @@ public class Day3 {
       return Long.parseLong(nums[0]) * Long.parseLong(nums[1]);
     }
 
-    public long part2() { return 0; }
+    public long part2() {
+      Pattern doing = Pattern.compile("do\\(\\)");
+      Pattern donting = Pattern.compile("don't\\(\\)");
+      Pattern mul = Pattern.compile("mul\\(\\d{1,3}?,\\d{1,3}?\\)");
+      long total = 0;
+      int mulIndex, doIndex, dontIndex;
+      doIndex = dontIndex = mulIndex = 0;
+
+      int startIndex = 0, endIndex;
+      boolean toDo = true;
+      for (int i = 0; i < inputs.size(); i++) {
+        String input = inputs.get(i);
+        Matcher mulMatch = mul.matcher(input);
+        Matcher doMatch = doing.matcher(input);
+        Matcher dontMatch = donting.matcher(input);
+
+        endIndex = input.length();
+        if (doMatch.find()) {
+          doIndex = doMatch.start();
+        } else {
+          doIndex = endIndex;
+        }
+        if (dontMatch.find()) {
+          dontIndex = dontMatch.start();
+        } else {
+          dontIndex = endIndex;
+        }
+        if (mulMatch.find()) {
+          mulIndex = mulMatch.start();
+        } else {
+          mulIndex = endIndex;
+        }
+
+        //like a merge sort
+        //keep 3 pointers
+        do {
+          //find the least index
+          if (doIndex < dontIndex && doIndex < mulIndex) {
+            toDo = true;
+            if (doMatch.find()) {
+              doIndex = doMatch.start();
+            } else {
+              doIndex = endIndex;
+            }
+          } else if (dontIndex < doIndex && dontIndex < mulIndex) {
+            toDo = false;
+            if (dontMatch.find()) {
+              dontIndex = dontMatch.start();
+            } else {
+              dontIndex = endIndex;
+            }
+          } else if (mulIndex < doIndex && mulIndex < dontIndex) {
+            if (toDo) {
+              total += mult(mulMatch.group());
+            }
+
+            if (mulMatch.find()) {
+              mulIndex = mulMatch.start();
+            } else {
+              mulIndex = endIndex;
+            }
+          }
+        } while(doIndex < endIndex || dontIndex < endIndex || mulIndex < endIndex);
+      }
+      return total;
+    }
 }
