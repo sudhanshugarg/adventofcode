46f4dd6a0769fdd4ce05b78461f1ffad28739687
Sudhanshu Garg <sudhanshug@gmail.com>
2024-12-15 10:23:17 -0800

    initial python template for day12 onwards

---
+++ 2024/python_template.py
@@ -0,0 +1,29 @@
+import json
+import logging
+import sys
+import time
+import os
+from typing import List
+
+class DayX:
+    def __init__(self, filename: str):
+        with open(filename, 'r') as f:
+            self.input = f.readlines()
+
+    def part1(self):
+        return len(self.input)
+
+    def part2(self):
+        return -1
+
+
+def run(args):
+    inputFileName = args[1]
+
+    day = DayX(inputFileName)
+    print(day.part1())
+    print(day.part2())
+
+
+if __name__ == "__main__":
+    run(sys.argv)
